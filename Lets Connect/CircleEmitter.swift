9b96a2dd0b78c27a8de64b8fe9dfbb9217125c0b
NazarStf <hworker98@gmail.com>
2023-07-29 19:26:59 +0300

    Create CircleEmitter

---
+++ Lets Connect/CircleEmitter.swift
@@ -0,0 +1,29 @@
+//
+//  CircleEmitter.swift
+//  Lets Connect
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+struct CircleEmitter: View {
+	@Binding var isAnimating: Bool
+	
+	var body: some View {
+		Circles()
+			.fill(Color.white.opacity(0.75))
+			.scaleEffect(isAnimating ? 2 : 0)
+			.animation(isAnimating ? Animation.spring() : Animation.easeInOut(duration: 0), value: isAnimating)
+	}
+}
+
+struct CircleEmitter_Previews: PreviewProvider {
+	static var previews: some View {
+		ZStack {
+			Color.black.edgesIgnoringSafeArea(.all)
+			CircleEmitter(isAnimating: .constant(true))
+		}
+			
+	}
+}
