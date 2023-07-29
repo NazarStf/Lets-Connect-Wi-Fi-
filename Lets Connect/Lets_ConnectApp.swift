8ea2c3b8dc6d7082a1aee9f840e9977a1770b2e7
NazarStf <hworker98@gmail.com>
2023-07-29 19:24:53 +0300

    Initial Commit

---
+++ Lets Connect/Lets_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lets_ConnectApp.swift
+//  Lets Connect
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lets_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
